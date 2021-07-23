0a33ea2d49674f6c834bc620afb9d467d050230f
HP <nikki_leong_niqi@connect.ite.edu.sg>
2021-07-23 09:11:44 +0800

    finish lab

--- demoVcs.py
+++ demoVcs.py
@@ -1,2 +1,16 @@
-print('testing')
-print(3+5)
+class Calculator:
+    def add(num1,num2):
+        add = num1 + num2
+        print('Sum is',add)
+        return add
+    def minus(num1,num2):
+        total2 = num1 - num2
+        print('Sum is ',total2)
+        return total2
+    def multiply(num1,num2):
+        total3 = num1 * num2
+        print('Sum is ', total3)
+
+Calculator.add(50,40)
+Calculator.minus(50,40)
+Calculator.multiply(50,40)
